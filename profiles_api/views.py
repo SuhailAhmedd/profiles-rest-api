5e755edd0406d7f41c63f7013c236c0c24050e95
Suhail <suhailahmedd03@gmail.com>
2024-07-11 18:27:19 +0530

    Added user profile feed API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,24 +6,27 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 from profiles_api import serializers
 from profiles_api import models
 from profiles_api import permissions
 
+
 class HelloApiView(APIView):
-    """Test API VIEW"""
+    """Test API View"""
     serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
         an_apiview = [
             'Uses HTTP methods as function (get, post, patch, put, delete)',
-            'Is similar to traditional Django View',
-            'Gives you the most control over your application logic',
-            'Is mapped manually to the URLs',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over you application logic',
+            'Is mapped manually to URLs',
         ]
-        return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
 
     def post(self, request):
         """Create a hello message with our name"""
@@ -34,7 +37,10 @@ class HelloApiView(APIView):
             message = f'Hello {name}'
             return Response({'message': message})
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
     def put(self, request, pk=None):
         """Handle updating an object"""
@@ -48,6 +54,7 @@ class HelloApiView(APIView):
         """Delete an object"""
         return Response({'method': 'DELETE'})
 
+
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet"""
     serializer_class = serializers.HelloSerializer
@@ -55,10 +62,11 @@ class HelloViewSet(viewsets.ViewSet):
     def list(self, request):
         """Return a hello message"""
         a_viewset = [
-            'Uses actions (list, create, retrieve, update, partial_update)',
-            'Automatically maps to URLs using routers',
+            'Uses actions (list, create,retrieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
             'Provides more functionality with less code',
         ]
+
         return Response({'message': 'Hello!', 'a_viewset': a_viewset})
 
     def create(self, request):
@@ -70,7 +78,10 @@ class HelloViewSet(viewsets.ViewSet):
             message = f'Hello {name}!'
             return Response({'message': message})
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
     def retrieve(self, request, pk=None):
         """Handle getting an object by its ID"""
@@ -98,6 +109,19 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
 
+
 class UserLoginApiView(ObtainAuthToken):
-    """Handle creating user authentication token ."""
+    """Handle creating user authentication tokens"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.UpdateOwnStatus, IsAuthenticated)
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&#ifl((-qx$9d*+o(p@j9uo#849m-+&9*=nb4=agyn=jg_993('
+SECRET_KEY = 'l+2fr-rwd31cqz(vct^(!r(thlu#meb5_d0i8jslx8jpn5ltk-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-18-212-138-237.compute-1.amazonaws.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -123,3 +126,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
